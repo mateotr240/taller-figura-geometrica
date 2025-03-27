f3f214783435889dba3699b4cf94f3a212a27728
mateotr240 <mateotr24033@gmail.com>
2025-03-27 08:40:40 -0500

    Update and rename PARCIAL_DOOM.py to taller python.py

---
+++ taller python.py
@@ -0,0 +1,15 @@
+opciones = {
+     
+}
+
+#introduce datos de entrada
+while True
+input("ingrese vertice x: ")
+input("ingrese vertice y: ")
+input("ingrese datos de z: ")
+y = input 
+x = input 
+z = input
+#ingrese la cantidad de caras
+input("caras ingresadas: ")
+F= input
